fd15703f4e5dab3fd1559d01439e229a326555d5
StratosKakalis <stratoskakalis123@gmail.com>
2024-03-22 17:55:00 +0200

    simple test inference with hugginface llama

---
+++ Llama2/main.py
@@ -0,0 +1,46 @@
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="cuda",
+)
+
+sequences = pipeline(
+    'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
+
+sequences = pipeline(
+    'Only respond with SPARQL code. Write a SQARQL query equivalent to this natural language question: "What is the population of Central Greece?"\n',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
+
+sequences = pipeline(
+    'Convert the sentence: "Where is Liverpool located?" into a GeoSPARQL query.\n',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
